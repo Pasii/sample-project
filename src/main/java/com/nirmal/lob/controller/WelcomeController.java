9382e372d4409f48e35f9dd96657988fdf7a1000
Pasindu Deeyagahage <pdeeyagahage@virtusapolaris.com>
2017-06-19 13:37:11 +0530

    somthing

--- src/main/java/com/nirmal/lob/controller/WelcomeController.java
+++ src/main/java/com/nirmal/lob/controller/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 	
-	@RequestMapping(value="welcome")
+	@RequestMapping(value="/welcome")
 	public String getWelcome(Model model){
 		model.addAttribute("greeting","Hi nirmal eka wada");
 		return "welcome";
